363fff807161c1094dff167a03e3971e86887cc2
bleso <bleso624@gmail.com>
2021-09-29 20:04:58 +0800

    initial project

---
+++ CHZU.OpenJudger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CHZU.OpenJudger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
